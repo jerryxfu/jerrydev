efd724d7c5471c4172ac871060bdae753198ed44
Jerry <jerryfu_mc@outlook.com>
2026-05-20 22:16:26 -0400

    cleanup

--- src/pages/HomePage/About/About.tsx
+++ src/pages/HomePage/About/About.tsx
@@ -6,13 +6,11 @@ export default function About() {
         <div className="section about">
             <SectionTitle text={"About"} />
             <div className="layout-row">
-                <p className="text" /*style={{maxWidth: "70%"}}*/>
+                <p className="text">
                     Hi, I'm Jerry, an aspiring physician-engineer in IB Diploma Programme in Pure and Applied Sciences. I'm passionate about science,
                     AI, medicine, and technology. This website is a collection of my personal projects, experiments, and interests. Feel free to
                     explore and reach out if you have any questions or just want to say hi!
                 </p>
-                {/*<div className="container">*/}
-                {/*</div>*/}
             </div>
         </div>
     );
