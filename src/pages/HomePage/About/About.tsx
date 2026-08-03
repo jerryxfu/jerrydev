d399edfcd3adeab39e1ce60e7e6cb7f93efe6384
Jerry <jerryfu_mc@outlook.com>
2026-08-03 18:06:47 -0400

    test fix againn

--- src/pages/HomePage/About/About.tsx
+++ src/pages/HomePage/About/About.tsx
@@ -7,7 +7,8 @@ export default function About() {
             <SectionTitle text={"About Me"} />
             <div className="layout-row">
                 <p className="text-body">
-                    Hi, I'm Jerry and I do things. Here are some things I find worth sharing.
+                    Hi, I'm Jerry and I do things. Here are some things I find worth sharing. Feel free to reach out if you have any ideas or
+                    suggestions!
                     {/*Hi, I'm Jerry — a student and developer building intelligent systems at the intersection of robotics, medicine, and AI. */}
                 </p>
             </div>
